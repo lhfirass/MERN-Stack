4fde3c109f7f18b085d7a704523e66826a79087a
lhfirass <81319618+lhfirass@users.noreply.github.com>
2025-09-09 14:54:24 +0100

    Update: ad contollers

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -3,35 +3,18 @@
 // todo: and then i need to define my routers
 
 const express = require('express');
-const Workout = require('../models/workoutModel');
+const { createWorkout, getWorkout, getWorkouts, deleteWorkout, updateWorkout } = require('../controllers/workoutController')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json('Get all workouts');
-});
+router.get('/', getWorkouts);
 
-router.get('/:id', (req, res) => {
-    res.json('Get a single workout');
-});
+router.get('/:id', getWorkout);
 
-router.post('/', async (req, res) => {
-    const { title, load, reps } = req.body;
+router.post('/', createWorkout);
 
-    try {
-        const workout = await Workout.create({ title, load, reps });
-        res.status(200).json(workout);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
+router.delete('/:id', deleteWorkout);
 
-router.delete('/:id', (req, res) => {
-    res.json('Delete a workout');
-});
-
-router.patch('/:id', (req, res) => {
-    res.json('Update a workout');
-});
+router.patch('/:id', updateWorkout);
 
 module.exports = router;
